1422142be6e3ee161dd6dfb906d1b1b7b4e2d0b2
Dashinnnn <yagodamaratas28@gmail.com>
2025-02-24 14:44:36 +0800

    Faculty register and login CSS updated

--- connection/config.php
+++ connection/config.php
@@ -6,7 +6,7 @@ $password = "";
 $database = "ojtportal";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=$database;port=$port;", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
--- coordinator/index.php
+++ coordinator/index.php
@@ -22,22 +22,28 @@ session_start();
             text-align: center;
             padding: 2rem;
             background: url(./images/itech.jpg);
+            background-color: rgba(255, 255, 255, 0.39);
+            background-blend-mode: overlay;
             height: 94vh;
             background-attachment: fixed;
             background-position: center;
             background-repeat: no-repeat;
             background-size: cover;
             overflow: hidden;
+            display: flex;
+            align-items: center;
+            justify-content: center;
         }
         
         h5{
             font-size: 18px;
-            font-weight: 400;
-            margin-bottom: 20px;  
+            font-weight: 600;
+            margin-bottom: 50px;  
             align-items: center;
             justify-content: center;
             display: flex;
             line-height: 0.5;
+            color: #9B0C0C;
         }
         i {
             font-size: 16px;
@@ -48,36 +54,179 @@ session_start();
             display: flex;
             line-height: 0.5;
         }
+
+        input {
+          width: 500px;
+          height: 50px;
+          padding: 10px;
+          margin: 10px 0;
+          border: 1px solid #000;
+          border-radius: 5px;
+          outline: none;
+          color: #000;
+          font-size: 14px;
+        }
+
+        .nav-hte {
+          font-family: 'Source Serif 4', serif;
+          background: linear-gradient(to left, rgba(155, 12, 12, 1), rgba(255, 255, 255, 1));
+          color: rgb(141, 9, 9);
+          padding: 15px 0;
+          text-align: left;
+          font-size: 20px;
+          font-weight: 400;
+          position: fixed;
+          top: 0;
+          right: 0;
+          width: 100%;
+          display: flex;
+          align-items: center;
+          margin: 0 auto;
+          background-clip: padding-box;
+        }
+
+        .nav-logo {
+          height: 50px;
+          margin-left: 20px;
+        }
+
+        .nav-title {
+          font-size: 24px;
+          font-weight: bold;
+        }
+
+        .nav-caption {
+          font-size: 16px;
+          color: #000;
+          font-weight: normal;
+        }
+
+        .nav-title-caption-container {
+          display: flex;
+          flex-direction: column;
+          margin-left: 20px;
+        }
+
+        .login-section {
+            background: #fff;
+            width: 850px;
+            padding: 20px;
+            border-radius: 15px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            margin-top: 100px;
+            text-align: left;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            line-height: 1.5;
+        }
+
+        .back-button {
+          display: flex;
+          align-items: center;
+          background: none;
+          color: rgba(128, 128, 128, 0.5);
+          padding: 10px 20px;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+          font-size: 16px;
+          font-weight: 400;
+          transition: 0.3;
+          position: relative;
+          right: 185px;
+          bottom: 15px;
+        }
+
+        .back-button img {
+          height: 40px;
+          filter: grayscale(100%);
+          opacity: 0.3;
+          transition: filter 0.3s ease, opacity 0.3s ease;
+        }
+
+        .back-button:hover,
+        .back-button:hover img {
+          color: #9B0C0C;
+          filter: grayscale(0%) sepia(100%) hue-rotate(330deg) saturate(500px);
+          opacity: 1;
+        }
+
+        .header {
+          display: flex;
+          align-items: center;
+          gap: 50px;
+        }
+
+        .login-button {
+          width: 500px;
+          height: 50px;
+          background-color: #9B0C0C;
+          color: #fff;
+          padding: 10px 20px;
+          border: none;
+          border-radius: 5px;
+          cursor: pointer;
+          font-size: 18px;
+          font-weight: 600;
+          margin-top: 10px;
+          transition: 0.3s;
+        }
+
+        .forgot-password {
+            font-size: 16px;
+            color: #9B0C0C;
+        }
+
+        .register-here {
+            font-size: 16px;
+            color:#0C0C9B;
+        }
+
+        a {
+          align-self: center;
+          justify-self: center;
+          display: flex;
+        }
     </style>
 </head>
   <body>
-    <section class="u-clearfix u-section-1" id="sec-ae5f">
-      <div class="u-clearfix u-sheet u-sheet-1">
-        <div class="u-border-3 u-border-grey-75 u-container-style u-group u-radius-17 u-shape-round u-group-1">
-          <div class="u-container-layout u-container-layout-1">
-          <img class="u-image u-image-contain u-image-default u-image-1" src="images/pupLogo.png" alt="" style="max-height: 100px; align-items: center; justify-content: center; display: flex; margin: 10px auto;">
-            <h5>Polytechnic University of the Philippines</h5>
-            <i>Institute of Technology</i>
-            <h5>Supervisor Login</h5>
-            <div class="u-form u-form-1">
-            <form action="../php/coordinators_loginCode.php" method="POST">
-                <div class="u-form-group u-form-name u-label-none">
-                  <label for="studEMAIL" class="u-label">Email</label>
-                  <input type="email" placeholder="Enter your Email" id="studEMAIL" name="studEmail" class="u-border-2 u-border-black u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle u-input-1" required="">
+    <!-- Nav Bar -->
+    <nav class="nav-hte">
+      <img src="images/pupLogo copy.png" alt="pup logo" class="nav-logo">
+      <div class="nav-title-caption-container">
+        <div class="nav-title">Polytechnic University of the Philippines</div>
+        <div class="nav-caption">On the Job Training Portal</div>
+      </div>
+    </nav>
+
+      <!-- Login Container -->
+    <section id="sec-ae5f" class="login-section">
+      <div>
+        <div>
+          <div>
+            <div class="header">
+              <a href="\drills\index.php" class="back-button"><img src="images/less-than.png" alt="back icon" alt="">Back</a>
+              <h5>FACULTY ACCOUNT</h5>
+            </div>
+            <div>
+            <form action="../php/stud_loginCode.php" method="POST">
+                <div class="u-form-group u-form-name">
+                  <label for="studEMAIL" class="u-label">Username</label><br>
+                  <input type="email" placeholder="Enter Username" id="studEMAIL" name="studEmail" required="">
                 </div>
-                <div class="u-form-email u-form-group u-label-none">
-                  <label for="pword" class="u-label">Password</label>
-                  <input type="password" placeholder="Enter Password" id="pword" name="studPword" class="u-border-2 u-border-black u-border-no-left u-border-no-right u-border-no-top u-input u-input-rectangle u-input-2" required="">
+                <div class="u-form-group u-form-name">
+                  <label for="pword" class="u-label">Password</label><br>
+                  <input type="password" placeholder="Enter Password" id="studEMAIL" name="studEmail" required="">
                 </div>
-                <br>
                 <div class="u-align-center u-form-group u-form-submit">
-                <button name="LogIn" class="u-border-none u-btn u-btn-round u-btn-submit u-button-style u-grey-70 u-hover-palette-1-dark-1 u-radius-28 u-btn-1">Log In</button>
+                <button name="Login" class="login-button">Log In</button>
                 </div>
+                <p><a href="#" class="forgot-password">Forgot Password?</a></p>
+                <p><a href="coordinators_register.php" class="register-here">Register Here</a></p>
               </form>
               <br>
             </div>
-            <p class="u-text u-text-default u-text-1">Don't have account ? <a href="coordinators_register.php"> Sign Up Here</a></p>
-            <p class="u-text u-text-default u-text-2"><a href="#">Forgotten Password?</a></p>
           </div>
         </div>
       </div>
